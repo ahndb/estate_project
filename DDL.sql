c830f3ba03e3512bc8913519c168eb58d21172b6
ahndb <ahndb.dev@gmail.com>
2024-04-23 11:04:33 +0900

    240423 10:50 Create API

--- DDL.sql
+++ DDL.sql
@@ -17,7 +17,7 @@
 # Relationship
 # 사용자 - 이메일 인증번호 : 이메일 인증번호 테이블에 등록된 이메일만 사용자의 이메일로 사용할 수 있음 (1 : 1)
 # 사용자 - 게시물 : 사용자가 게시물을 작성할 수 있음 (1 : n)
-# 
+#
 # # - 사용자 (아이디*, 비밀번호, 이메일, 권한, 가입경로)
 # table name : user
 # user_id : VARCHAR(50) PK
@@ -25,18 +25,18 @@
 # user_email : VARCHAR(100) NN UQ FK email_auth_number(email)
 # user_role : VARCHAR(15) NN DEFAULT('ROLE_USER') CHECK('ROLE_USER', 'ROLE_ADMIN')
 # join_path : VARCHAR(5) NN DEFAULT('HOME') CHECK('HOME', 'KAKAO', 'NAVER')
-#  
+#
 # - 이메일 인증번호 (이메일*, 인증번호)
 #  table name : email_auth_number
 #  email : VARCHAR(100) PK
-#  auth_number : VARCHAR(4) NN 
+#  auth_number : VARCHAR(4) NN
 #
 # - 게시물 (접수번호*, 상태, 제목, 내용, 작성자, 작성일, 조회수,  답변)
 # table name : board
 # reception_number : INT PK AI
 # status : BOOLEAN NN DEFAULT(false)
-# title : VARCHAR(100) NN 
-# contents : TEXT NN 
+# title : VARCHAR(100) NN
+# contents : TEXT NN
 # writer_id : VARCHAR(50) NN FK user(user_id)
 # write_datetime : DATETIME NN DEFAULT(now())
 # view_count : INT NN DEFAULT(0)
@@ -44,12 +44,38 @@
 
 ## 데이터베이스 생성
 CREATE DATABASE estate;
+
 USE estate;
 
 ## 이메일 인증 번호 테이블 생성
 CREATE TABLE email_auth_number (
-  email VARCHAR(100) PRIMARY KEY,
-  auth_number VARCHAR(4) NOT NULL
+    email VARCHAR(100) PRIMARY KEY, auth_number VARCHAR(4) NOT NULL
 );
 
+## 유저 테이블 생성
+CREATE TABLE user (
+    user_id VARCHAR(50) PRIMARY KEY, 
+    user_password VARCHAR(255) NOT NULL, 
+    user_email VARCHAR(100) NOT NULL UNIQUE,
+    user_role VARCHAR(15) NOT NULL 
+        DEFAULT('ROLE_USER') 
+        CHECK (user_role IN ('ROLE_USER', 'ROLE_ADMIN')), 
+    join_path VARCHAR(5) NOT NULL 
+        DEFAULT('HOME') 
+        CHECK (join_path IN ('HOME', 'KAKAO', 'NAVER')), 
+    CONSTRAINT user_email_fk 
+        FOREIGN KEY (user_email) REFERENCES email_auth_number (email)
+);
 
+## Q&A 게시물 테이블 생성
+CREATE TABLE board (
+    reception_number INT PRIMARY KEY AUTO_INCREMENT, 
+    status BOOLEAN NOT NULL DEFAULT(false), 
+    title VARCHAR(100) NOT NULL,
+    contents TEXT NOT NULL,
+    writer_id VARCHAR(50) NOT NULL, 
+    write_datetime DATETIME NOT NULL DEFAULT(now()),
+    view_count INT NOT NULL DEFAULT(0),
+    CONSTRAINT writer_id_fk
+        FOREIGN KEY (writer_id) REFERENCES user (user_id)
+);
