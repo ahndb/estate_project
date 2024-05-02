4373ae77b21db76592a2b22ece4dd5ebce74c2c3
ahndb <ahndb.dev@gmail.com>
2024-05-02 11:52:52 +0900

    240502 11:50 Update QnaWrite

--- front/src/views/service/qna/QnaWrite/index.tsx
+++ front/src/views/service/qna/QnaWrite/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './style.css'
 
+
+//                    component                    //
 export default function QnaWrite() {
+
+  //                    render                  //
   return (
-    <div>QnaWrite</div>
+    <div id='qna-write-wrapper'>
+
+      <div className='qna-write-top'>
+        <div className='qna-write-title-box'>
+          <input type="text" placeholder='제목을 입력해주세요.' required />
+        </div>
+        <div>올리기</div>
+      </div>
+
+      <div>
+        <textarea placeholder='내용을 입력해주세요.' required />
+      </div>
+
+    </div>
   )
 }
