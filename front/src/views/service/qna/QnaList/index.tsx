3af2d0639838a509b61b36e3520feaee6629b815
ahndb <ahndb.dev@gmail.com>
2024-05-03 10:50:54 +0900

    240503 10:50 Update back

--- front/src/views/service/qna/QnaList/index.tsx
+++ front/src/views/service/qna/QnaList/index.tsx
@@ -1,8 +1,48 @@
 import React from 'react'
 import './style.css'
 
+//           component           //
 export default function QnaList() {
+
+  //           render          //
   return (
-    <div>QnaList</div>
+    <div>
+
+      <div>
+        <div>전체 <span>150건</span> | 페이지 <span>1/100</span></div>
+        <div>
+          <div>글쓰기</div>
+          <div>
+            <div>토글</div>
+            <div>미완료 보기</div>
+          </div>
+        </div>
+      </div>
+      
+      <div>
+        <div>
+          <div>접수번호</div>
+          <div>상태</div>
+          <div>제목</div>
+          <div>작성자</div>
+          <div>작성일</div>
+          <div>조회수</div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      
+      <div>
+        <div>1|2|3|4|5|6|</div>
+        <div>검색</div>
+      </div>
+
+    </div>
   )
 }
