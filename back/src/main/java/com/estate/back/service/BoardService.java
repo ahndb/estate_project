f955c77360d2ab2b9eed0f35ca35fee8d589076a
ahndb <ahndb.dev@gmail.com>
2024-05-07 16:49:23 +0900

    240507 17:50

--- back/src/main/java/com/estate/back/service/BoardService.java
+++ back/src/main/java/com/estate/back/service/BoardService.java
@@ -3,14 +3,17 @@ package com.estate.back.service;
 import org.springframework.http.ResponseEntity;
 
 import com.estate.back.dto.request.board.PostBoardRequestDto;
+import com.estate.back.dto.request.board.PostCommentRequestDto;
 import com.estate.back.dto.response.ResponseDto;
 import com.estate.back.dto.response.board.GetBoardListResponseDto;
 import com.estate.back.dto.response.board.GetBoardResponseDto;
 import com.estate.back.dto.response.board.GetSearchBoardListResponseDto;
 
+
 public interface BoardService {
   
   ResponseEntity<ResponseDto> postBoard(PostBoardRequestDto dto, String userId);
+  ResponseEntity<ResponseDto> postComment(PostCommentRequestDto requestBody, int receptionNumber);
   ResponseEntity<? super GetBoardListResponseDto> getBoardList();
   ResponseEntity<? super GetSearchBoardListResponseDto> getSearchBoardList(String searchWord);
   ResponseEntity<? super GetBoardResponseDto> getBoard(int receptionNumber);
