7ac6680b016645c4e90842a9b57443ab5f130778
ahndb <ahndb.dev@gmail.com>
2024-05-09 10:50:54 +0900

    240509 10:50 Update local

--- front/src/views/service/Local/index.tsx
+++ front/src/views/service/Local/index.tsx
@@ -1,12 +1,54 @@
 import React from 'react'
 import './style.css'
-import { Chart } from 'chart.js'
+
 
 //                    component                    //
 export default function NotFound() {
   //                    render                    //
   return (
+    <div id='local-wrapper'>
 
-    <Chart />
+      <div className='local-top'>
+        <div className='local-search-box'>
+          <div className='select-box'>
+            <div className='select-selected'></div>
+            <div className='select-open-button'></div>
+          </div>
+          <div className='primary-button'>검색</div>
+        </div>
+        <div className='local-origin-text'>데이터 출처: KOSIS</div>
+      </div>
+
+      <div className='local-card'>
+        <div className='local-card-title-box'>
+          <div className='local-card-title'>매매평균</div>
+          <div className='local-card-unit'>(단위: 백만원)</div>
+        </div>
+        <div className='local-card-chart-box'>
+          <>차트칸</>
+        </div>
+      </div>
+
+      <div className='local-card'>
+        <div className='local-card-title-box'>
+          <div className='local-card-title'>전세 평균</div>
+          <div className='local-card-unit'>(단위: 백만원)</div>
+        </div>
+        <div className='local-card-chart-box'>
+          <>차트칸</>
+        </div>
+      </div>
+
+      <div className='local-card'>
+        <div className='local-card-title-box'>
+          <div className='local-card-title'>월세 평균</div>
+          <div className='local-card-unit'>(단위: 백만원)</div>
+        </div>
+        <div className='local-card-chart-box'>
+          <>차트칸</>
+        </div>
+      </div>
+
+    </div>
   )
 }
